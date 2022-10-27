b056b769385c6a909f39e34893e20b0020e91292
RicPra <114755903+RicPra@users.noreply.github.com>
2022-10-27 16:44:22 +0200

    Update R_code_pointPattern.r

--- R_code_pointPattern.r
+++ R_code_pointPattern.r
@@ -1,3 +1,23 @@
-# Point pattrn analysis for population ecology
+# Point pattern analysis for population ecology
 
-# Use the
+# We have to install a new package now called "spatstat"
+install.packages("spatstat")
+# And then we recall it in R
+library(spatstat)
+
+# Use of working directory
+setwd("/Users/Ricky/Documents/MONITORING/LAB")
+
+# Read and save our data
+covid <- read.table("covid_agg.csv", header=TRUE)
+
+# We should explain to R that we have dimensional points and that it has to pair them
+# Let's use a function called "ppp" to create a planar point pattern
+# We should say which are the coordinates (lon and lat) and say the range of the data
+attach(covid)
+covid_planar <- ppp(x=lon, y=lat, c(-180,180), c(-90,90))
+# Note we have assigned an object to the ppp function, and first we have attached covid (or we had to use i.e. "x=covid$lon")
+
+# Try to plot the data
+plot(covid_planar)
+
