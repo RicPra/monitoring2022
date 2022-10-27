66bf12c2c9094e25fa370f0605915950dc7d3076
RicPra <114755903+RicPra@users.noreply.github.com>
2022-10-27 17:03:41 +0200

    Update R_code_pointPattern.r

--- R_code_pointPattern.r
+++ R_code_pointPattern.r
@@ -12,7 +12,7 @@ setwd("/Users/Ricky/Documents/MONITORING/LAB")
 covid <- read.table("covid_agg.csv", header=TRUE)
 
 # We should explain to R that we have dimensional points and that it has to pair them
-# Let's use a function called "ppp" to create a planar point pattern
+# Let's use a function called "ppp()" to create a planar point pattern
 # We should say which are the coordinates (lon and lat) and say the range of the data
 attach(covid)
 covid_planar <- ppp(x=lon, y=lat, c(-180,180), c(-90,90))
@@ -21,3 +21,21 @@ covid_planar <- ppp(x=lon, y=lat, c(-180,180), c(-90,90))
 # Try to plot the data
 plot(covid_planar)
 
+# Let's now make a map of density of points, using the function "density()" and assign it to an object
+density_map <- density(covid_planar)
+# And try to plot the data, adding the original points
+plot(density_map)
+points(covid_planar, pch=15)
+
+# We can change the colors of our map. The number at the end says how many colors you want in your palette
+# We assign our color plaette to a name so we can use later
+cl <- colorRampPalette(c("cyan", "coral", "chartreuse"))(100)
+
+# Now we try our palette
+plot(density_map, col=cl)
+points(covid_planar, pch=17, col="blue")
+
+# Trying a different palette hihi
+clr <- colorRampPalette(c("aliceblue", "cadetblue1", "cornflowerblue", "darkblue"))(200)
+plot(density_map, col=clr)
+points(covid_planar, pch=11, col="darkorange1")
