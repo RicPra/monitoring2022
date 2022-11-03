5c7fd9de82cd8124970c34d864631b7a0341d7db
RicPra <114755903+RicPra@users.noreply.github.com>
2022-11-03 17:35:08 +0100

    Create R_code_multivariate.r

---
+++ R_code_multivariate.r
@@ -0,0 +1,9 @@
+# Community ecology
+# We are going to make multivariate analysis
+
+# We install a new package called "vegan()" which means VEGetation ANalysis
+install.packages("vegan")
+
+library(vegan)
+
+# We should upload some data using the path of our directory as ususal
