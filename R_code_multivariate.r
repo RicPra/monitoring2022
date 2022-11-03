c632d7a07b2960a59153dd814f8d2fc4048ba446
RicPra <114755903+RicPra@users.noreply.github.com>
2022-11-03 17:58:34 +0100

    Update R_code_multivariate.r

--- R_code_multivariate.r
+++ R_code_multivariate.r
@@ -7,3 +7,23 @@ install.packages("vegan")
 library(vegan)
 
 # We should upload some data using the path of our directory as ususal
+setwd("/Users/Ricky/Documents/MONITORING/LAB")
+
+# To read the data we are not using the function "read.data()" as usual
+# We use the "load()" function that reloads saved datasets
+load("biomes_multivar.RData")
+
+# Now that the project is completely imported we use the "ls()" function to see what files are inside
+ls()
+
+# We use the function "decorana()" which performs detrended correspondence analysis
+multivar <- decorana(biomes)
+# This squeezes in just two axes all the plots of the table "biomes"
+# Let's see
+plot(multivar)
+
+# We have taken a plot of 20 species and we can see all of them in only two axes
+# But how to see all the biomes? We have a label in a different table that gives all the informations we need
+attach(biomes_types)
+# We use a function called "ordiellipse()" which ordinate plots in a new space
+ordiellipse(multivar, type, col=c("black", "red", "green", "blue"))
