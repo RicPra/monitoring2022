89df67610eab8f0a2fe4d777f9296de52c44a54a
RicPra <114755903+RicPra@users.noreply.github.com>
2022-11-10 17:15:11 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -5,3 +5,13 @@ install.packages("raster")
 library(raster)
 
 # As usual we set the working directory
+setwd("/Users/Ricky/Documents/MONITORING/LAB")
+
+# And we are going to upload the data, using a function called "brick()", telling it the name of the file we need
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+# We assigned the function to an object obv
+
+# We can plot the single bands in 7 different plots to see how they are
+plot(p224r63_2011)
+
+# Let's change color, using the palette we used already
