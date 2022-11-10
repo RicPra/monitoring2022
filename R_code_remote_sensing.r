46155541fbaba7b9421868144a4e4955faba38b4
RicPra <114755903+RicPra@users.noreply.github.com>
2022-11-10 16:51:48 +0100

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,7 @@
+# R code for remote sensing data analysis in ecosystem monitoring
+
+# We are using the "raster" package
+install.packages("raster")
+library(raster)
+
+# As usual we set the working directory
