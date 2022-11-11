0ed0343ba7a4e6384d2aee14e1a4e661dc16ed3c
RicPra <114755903+RicPra@users.noreply.github.com>
2022-11-11 17:40:16 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -8,6 +8,7 @@ library(raster)
 setwd("/Users/Ricky/Documents/MONITORING/LAB")
 
 # And we are going to upload the data, using a function called "brick()", telling it the name of the file we need
+# The "brick()" function is importing all the data all together
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 # We assigned the function to an object obv
 
@@ -18,3 +19,17 @@ plot(p224r63_2011)
 cl <- colorRampPalette(c("cyan", "coral", "chartreuse"))(100)
 plot(p224r63_2011, col=cl)
 
+# To see only the plot of the first band (blue) we do like this
+plot(p224r63_2011$B1_sre, col=cl)
+# If we know the position of the band we can do so, with the quadratic parentesis, using the position and not the name
+plot(p224r63_2011[[1]], col=cl)
+
+# Now we try to change some colors xdxd
+clr <- colorRampPalette(c("dodgerblue4", "cornflowerblue", "aliceblue"))(200)
+plot(p224r63_2011[[1]], col=clr)
+
+# If we want to plot one image beside the other we do like this
+par(mfrow=c(1,2))
+# This is an array with columns and rows, prepared to recive data inside of it
+plot(p224r63_2011[[1]], col=clr)
+plot(p224r63_2011[[2]], col=cl)
