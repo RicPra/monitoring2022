8afdfd823a42d6fa3fb738307f65a3b62d57abf0
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-01 17:06:08 +0100

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,3 +1,13 @@
+# We download and analyse Copernicus data
 
+# Copernicus set: https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+# We need a new package called "ncdf4" to read .nc files
 install.packages("ncdf4")
+# And we recall all the packages we need
 library(ncdf4)
+library(raster)
+library(ggplot2)
+library(RStoolbox)
+library(viridis)
+library(patchwork)
