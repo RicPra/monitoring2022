1534a646ce938ee7d47b7082a7a4881c9bbcb324
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-01 17:18:33 +0100

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,4 +1,5 @@
 # We download and analyse Copernicus data
+# In particular we analyse the snow cover
 
 # Copernicus set: https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
 
@@ -11,3 +12,9 @@ library(ggplot2)
 library(RStoolbox)
 library(viridis)
 library(patchwork)
+
+# Now we import the data, as usual setting our work directory
+setwd("/Users/Ricky/Documents/MONITORING/LAB")
+
+# We use this function called "raster()" from the raster package
+snow <- raster("c_gls_SCE_202012210000_NHEMI_VIIRS_V1.0.1.nc")
