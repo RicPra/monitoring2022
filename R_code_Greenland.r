032d3068783b7ed7855506e7127db90b2c96d3e0
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-02 16:29:48 +0100

    Update R_code_Greenland.r

--- R_code_Greenland.r
+++ R_code_Greenland.r
@@ -2,9 +2,12 @@
 
 # Packages we need
 library("raster")
+library("RStoolbox")
+library(ggplot2)
 
 # We import the data we need, setting the working directory
 setwd("/Users/Ricky/Documents/MONITORING/LAB")
 lst_2000 <- raster("lst_2000.tif")
 
 # Now we plot it with ggplot()
+ggplot() + geom_raster(snow, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="mako")
