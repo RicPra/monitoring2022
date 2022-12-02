d5446adba80796eb26de71f4caae7800205ba106
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-02 16:28:23 +0100

    Update R_code_Greenland.r

--- R_code_Greenland.r
+++ R_code_Greenland.r
@@ -1,4 +1,10 @@
-# Study of the ice melting in Greenland
+# Study of the ice melting in Greenland due to increasing temperature
+
+# Packages we need
 library("raster")
 
-# We import the data we need
+# We import the data we need, setting the working directory
+setwd("/Users/Ricky/Documents/MONITORING/LAB")
+lst_2000 <- raster("lst_2000.tif")
+
+# Now we plot it with ggplot()
