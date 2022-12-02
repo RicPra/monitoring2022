f0711d80a8e326fb8134c0590e1c2f07ab757bb1
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-02 16:36:07 +0100

    Update R_code_Greenland.r

--- R_code_Greenland.r
+++ R_code_Greenland.r
@@ -1,13 +1,16 @@
 # Study of the ice melting in Greenland due to increasing temperature
 
 # Packages we need
-library("raster")
-library("RStoolbox")
+library(raster)
 library(ggplot2)
+library(RStoolbox)
+library(viridis)
+
 
 # We import the data we need, setting the working directory
 setwd("/Users/Ricky/Documents/MONITORING/LAB")
 lst_2000 <- raster("lst_2000.tif")
 
-# Now we plot it with ggplot()
-ggplot() + geom_raster(snow, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) + scale_fill_viridis(option="mako")
+# Now we plot it with ggplot(), using the raster geometry
+ggplot() + geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) + scale_fill_viridis(option="inferno")
+
