65498bae2fed6dd1f079065897363dc5b95e7701
RicPra <114755903+RicPra@users.noreply.github.com>
2023-01-22 19:18:35 +0100

    Update R_code_examProject.r

--- R_code_examProject.r
+++ R_code_examProject.r
@@ -1,1 +1,17 @@
 # My exam project xdxd
+
+library(ncdf4)
+library(raster)
+library(ggplot2)
+library(RStoolbox)
+library(viridis)
+library(patchwork)
+
+
+setwd("/Users/Ricky/Documents/MONITORING/examProject_RiccardoPrandi")
+
+temp <- raster("c_gls_LST_201010200000_GLOBE_GEO_V1.2.1.nc")
+
+ggplot() + geom_raster(temp, mapping = aes(x=x, y=y, fill=LST.Error.Bar)) + scale_fill_viridis(option="mako")
+
+# I have no idea what I have done
