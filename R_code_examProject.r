b4db61569db77222e8a70505a23339da31dd0517
RicPra <114755903+RicPra@users.noreply.github.com>
2023-01-31 01:25:27 +0100

    Update R_code_examProject.r

--- R_code_examProject.r
+++ R_code_examProject.r
@@ -10,19 +10,22 @@ library(patchwork)
 
 setwd("/Users/Ricky/Documents/MONITORING/examProject_RiccardoPrandi")
 
-temp2010 <- raster("c_gls_LST_201001180000_GLOBE_GEO_V1.2.1.nc", varname="LST")
+temp2010 <- raster("c_gls_LST_201007031300_GLOBE_GEO_V1.2.1.nc", varname="LST")
 
-temp2014 <- raster("c_gls_LST_201412100100_GLOBE_GEO_V1.2.1.nc", varname="LST")
+temp2015 <- raster("c_gls_LST_201507031300_GLOBE_GEO_V1.2.1.nc", varname="LST")
 
-temp2021 <- raster("c_gls_LST_202101181300_GLOBE_GEO_V1.2.1.nc", varname="LST")
+temp2020 <- raster("c_gls_LST_202007031300_GLOBE_GEO_V1.2.1.nc", varname="LST")
 
 
 
 p1 <- ggplot() + geom_raster(temp2010, mapping = aes(x=x, y=y, fill=Land.Surface.Temperature )) + scale_fill_viridis(option="mako")
 
-p2 <- ggplot() + geom_raster(temp2014, mapping = aes(x=x, y=y, fill=Land.Surface.Temperature )) + scale_fill_viridis(option="mako")
+p2 <- ggplot() + geom_raster(temp2015, mapping = aes(x=x, y=y, fill=Land.Surface.Temperature )) + scale_fill_viridis(option="mako")
 
-p3 <- ggplot() + geom_raster(temp2021, mapping = aes(x=x, y=y, fill=Land.Surface.Temperature )) + scale_fill_viridis(option="mako")
+p3 <- ggplot() + geom_raster(temp2020, mapping = aes(x=x, y=y, fill=Land.Surface.Temperature )) + scale_fill_viridis(option="mako")
 
 
+ext <- c(-2, 10, 20, 30)
+temp2020europe <- crop(temp2020, ext)
 # I have no idea what I have done
+
