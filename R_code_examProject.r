36e33567bc03f10787acddce92332efe5ae0ea4b
RicPra <114755903+RicPra@users.noreply.github.com>
2023-06-14 23:58:57 +0200

    Update R_code_examProject.r

--- R_code_examProject.r
+++ R_code_examProject.r
@@ -2,9 +2,9 @@
 # Analysis of the snow cover on the Adamello Presanella group on the Italian Alps
 
 # With this project I want to look at the variation of the ice cover at the beginning of the summer from
-# May/August 2001 to May/August 2022
+# May/August 2001 to May/August 2029
 
-# The images are a median of the period from the beginning of May to end of August of 2001, 2010, and 2022.
+# The images are a median of the period from the beginning of May to end of August of 2001, 2010, and 2019.
 
 library(raster)
 library(ggplot2)
@@ -19,7 +19,8 @@ adamello2001 <- brick("landsat2001Adamello.tif")
 
 adamello2010 <- brick("landsat2010Adamello.tif")
 
-adamello2022 <- brick("landsat2022Adamello.tif")
+adamello2019 <- brick("landsat2019Adamello.tif")
+
 
 # for landsat the sr_b1 = blue etc..vedi roba rocio (3,2,1 perchè le bande di landsat sono così
 
@@ -28,37 +29,36 @@ plotRGB(adamello2001, r=3, g=2, b=1, stretch="hist")
 
 plotRGB(adamello2010, r=3, g=2, b=1, stretch="hist")
 
-plotRGB(adamello2022, r=3, g=2, b=1, stretch="hist")
+plotRGB(adamello2019, r=3, g=2, b=1, stretch="hist")
 
 
 ext <- c(601875, 641215, 5090355, 5130000)
 # We use the function "crop()" and we save our new image
 adaPresa2001 <- crop(adamello2001, ext)
 adaPresa2010 <- crop(adamello2010, ext)
-adaPresa2022 <- crop(adamello2022, ext)
-
-
-plotRGB(adaPresa2001, r=3, g=2, b=1, stretch="hist")
-
-
-ggplot() + geom_raster(adaPresa2001, mapping = aes(x=x, y=y, fill=ST_B6)) + 
-scale_fill_viridis(option="mako", direction=1)
-
-ggplot() + geom_raster(adaPresa2010, mapping = aes(x=x, y=y, fill=ST_B6)) + 
-scale_fill_viridis(option="mako", direction=1)
-
-ggplot() + geom_raster(adaPresa2022, mapping = aes(x=x, y=y, fill=ST_B6)) + 
-scale_fill_viridis(option="mako", direction=1)
-
+adaPresa2019 <- crop(adamello2019, ext)
 
 par(mfrow=c(1,3))
+plotRGB(adaPresa2001, r=3, g=2, b=1, stretch="hist")
+plotRGB(adaPresa2010, r=3, g=2, b=1, stretch="hist")
+plotRGB(adaPresa2019, r=3, g=2, b=1, stretch="hist")
 
-plot(adamello2001)
-plot(adamello2010)
-plot(adamello2022)
+
+#con la temperatura
+p1 <- ggplot() + geom_raster(adaPresa2001, mapping = aes(x=x, y=y, fill=ST_B6)) + 
+scale_fill_viridis(option="mako", direction=1)
+
+p2 <- ggplot() + geom_raster(adaPresa2010, mapping = aes(x=x, y=y, fill=ST_B6)) + 
+scale_fill_viridis(option="mako", direction=1)
+
+p3 <- ggplot() + geom_raster(adaPresa2019, mapping = aes(x=x, y=y, fill=ST_B6)) + 
+scale_fill_viridis(option="mako", direction=1)
 
 
-# calassificando il ghiaccio, con due classi riesco a distinguere il ghiaccio da tutto il resto
+
+
+
+# calassificando il ghiaccio, con tre classi riesco a distinguere il ghiaccio da tutto il resto
 # set.seed tiene fermi i valori
 
 # r.code landcover rocio
@@ -83,6 +83,7 @@ freq(adaPresa2001class$map)
 #import <- lapply(rlist, raster)
 #adaPresa <- stack(import)
 
+#cambiare
 plot(adaPresa)
 
 #qua cambia le robe con il dollaro
